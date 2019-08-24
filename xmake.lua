79fe55715ef2a87962ad232fa83b64a802795eef
Phillip Chang <lasagnaphil@snu.ac.kr>
2019-08-24 19:05:06 +0900

    update xmake

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,6 @@
 target("altmath")
     add_includedirs("src", {public = true})
 
-target("altmath-simd")
-    add_includedirs("src", {public = true})
-
 test_srcs = {"tests/mat_test.cpp", "tests/quat_test.cpp", "tests/vec_test.cpp", "tests/test_main.cpp"}
 test_simd_srcs = {"tests/vec_simd_test.cpp"}
 
@@ -22,4 +19,4 @@ target("altmath-tests-simd")
     add_links("pthread")
     add_cxxflags("-msse", "-msse2", "-msse3", "-mavx", "-mfma")
     add_defines("ALTMATH_USE_SIMD")
-    add_deps("altmath-simd")
+    add_deps("altmath")
